e4cb4f4b1cb85c19ffdf48a6a94123dbd38c50fd
Takahiro Kato <dozo13189@gmail.com>
2026-05-30 13:38:04 +0900

    chore: fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ function assert(value: unknown): asserts value {
 }
 
 function validateCreateDate(createDate: string) {
-  if (createDate.trim() === "") {
+  if (!createDate) {
     return undefined;
   }
 
